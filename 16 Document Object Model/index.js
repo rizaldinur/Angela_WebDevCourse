a0950fbeb56fe4184516cb8adfa107ff259c36a8
rizaldinur <rnur71@gmail.com>
2024-05-10 17:22:44 +0700

    dom intro exercise

--- 16 Document Object Model/index.js
+++ 16 Document Object Model/index.js
@@ -1,1 +1,14 @@
-document.query
+var button = document.querySelector("button");
+var thirdList = document.querySelector("ul .list:last-child");
+var list = document.getElementsByTagName("li");
+
+list[1].style.color = "green";
+
+thirdList.innerHTML = "Ayam";
+thirdList.style.color = "red";
+
+button.style.backgroundColor = "yellow";
+
+var hugeHeading = document.querySelector("h1").classList;
+
+hugeHeading.toggle("huge");
