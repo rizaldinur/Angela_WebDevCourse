213cb969b59a758b67e2605cc78da816eec61e88
rizaldinur <rnur71@gmail.com>
2024-05-13 12:02:20 +0700

    drum kit add sound first try

--- 18 Drum Kit/index.js
+++ 18 Drum Kit/index.js
@@ -1,13 +1,24 @@
 //pemanggilan listener dg fungsi terpisah
-for (var i = 0; i < document.querySelectorAll("button").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-}
+// for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+// }
 
-function handleClick() {
-  alert("I got clicked ");
-}
+// function handleClick() {
+//   alert("I got clicked ");
+// }
 
 //pemanggilan listener function secara anonimus
 // document.querySelector("button").addEventListener("click", function () {
 //   alert("I got clicked ");
 // });
+
+//Play drum sounds
+
+var numberOfDrums = document.querySelectorAll("button").length;
+var drumSounds;
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    drumSounds = new Audio("./sounds/crash.mp3");
+    drumSounds.play();
+  });
+}
