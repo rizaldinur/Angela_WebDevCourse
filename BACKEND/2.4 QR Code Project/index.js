98deed7e383725205b68285224a1fb6212093fb2
rizaldinur <rnur71@gmail.com>
2024-05-23 15:00:51 +0700

    generate qr-image based on user input

--- BACKEND/2.4 QR Code Project/index.js
+++ BACKEND/2.4 QR Code Project/index.js
@@ -6,3 +6,24 @@
 
 // 1. Use the inquirer npm package to get user input.
 import inquirer from "inquirer";
+import * as qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      /* Pass your questions in here */
+      name: "URL",
+      message: "Enter URL Here: ",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    var qr_svg = qr.image(answers.URL, { type: "png" });
+    qr_svg.pipe(fs.createWriteStream("url.png"));
+    fs.writeFile("newURL.txt", answers.URL, function (err) {
+      if (err) throw err;
+    });
+
+    console.log(answers);
+  });
