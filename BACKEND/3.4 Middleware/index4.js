23339b8013497229991029694232ba665836c0c2
rizaldinur <rnur71@gmail.com>
2024-05-27 11:50:54 +0700

    handle band name post

--- BACKEND/3.4 Middleware/index4.js
+++ BACKEND/3.4 Middleware/index4.js
@@ -1,7 +1,28 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function getBandName(req, res, next) {
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+app.use(getBandName);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send("<h1>Your band name is:</h1>" + `<h2>${bandName}✌️</h2>`);
+  // console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
