8919302ae26b21afff81050fcac4a7e7c88d0398
rizaldinur <rnur71@gmail.com>
2024-05-27 11:45:27 +0700

    custom middleware

--- BACKEND/3.4 Middleware/index3.js
+++ BACKEND/3.4 Middleware/index3.js
@@ -3,13 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  console.log(res.statusCode);
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url} ${res.statusCode}`);
   next();
-});
+}
+
+app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello world");
 });
 
 app.listen(port, () => {
