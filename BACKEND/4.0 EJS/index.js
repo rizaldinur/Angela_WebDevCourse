e1c6230fe8d47b8b34b21a956be2d93c81b85ed6
rizaldinur <rnur71@gmail.com>
2024-05-29 17:43:02 +0700

    ejs trial

--- BACKEND/4.0 EJS/index.js
+++ BACKEND/4.0 EJS/index.js
@@ -7,11 +7,31 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+const date = new Date();
+const day = date.getDay();
+var dayType = "";
+var advice = "";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function getDayType(req, res, next) {
+  if (day < 5) {
+    dayType = "a weekday";
+    advice = "Let's work hard.";
+  } else {
+    dayType = "a weekend";
+    advice = "Let's rest our mind and body.";
+  }
+  next();
+}
+
+app.use(getDayType);
+
 app.get("/", (req, res) => {
-  const date = new Date();
-  res.render("index.ejs", { day: date.getDay() });
+  res.render("index.ejs", {
+    dayType: dayType,
+    advice: advice,
+  });
 });
 
 app.listen(port, () => {
