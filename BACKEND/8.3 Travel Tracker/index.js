5202312edb7de9cf12fd0666f1bb12c920930884
rizaldinur <rnur71@gmail.com>
2024-08-05 20:00:46 +0700

    fetch data visited to be shown

--- BACKEND/8.3 Travel Tracker/index.js
+++ BACKEND/8.3 Travel Tracker/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+// set up db connection
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "surabaya12",
+  port: 5432,
+});
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -8,7 +19,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  //fetch data from db
+  let data = await db.query("SELECT * from visited_countries");
+
+  let countries = [];
+  data.rows.forEach((element) => {
+    countries.push(element.country_code);
+  });
+  res.render("index.ejs", {
+    total: data.length,
+    countries: countries,
+  });
+  console.log(countries);
 });
 
 app.listen(port, () => {
