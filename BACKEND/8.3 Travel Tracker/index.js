e050d3f102acd8f6469066f92ad3258ad86c80c9
rizaldinur <rnur71@gmail.com>
2024-08-05 21:09:03 +0700

    handle add visited country route

--- BACKEND/8.3 Travel Tracker/index.js
+++ BACKEND/8.3 Travel Tracker/index.js
@@ -33,6 +33,31 @@ app.get("/", async (req, res) => {
   console.log(countries);
 });
 
+app.post("/add", async (req, res) => {
+  let country = req.body.country.trim().toLowerCase();
+
+  //get the country code
+  let data = await db.query("SELECT country_name, country_code from countries");
+  let found = data.rows.find(
+    (value) => value.country_name.trim().toLowerCase() === country
+  );
+
+  //cek if already visited
+  let visited = await db.query("SELECT country_code from visited_countries");
+  let filtered = [];
+  visited.rows.forEach((country) => {
+    filtered.push(country.country_code);
+  });
+  if (filtered.includes(found.country_code)) return res.redirect("/");
+
+  db.query("INSERT INTO visited_countries (country_code) values ($1)", [
+    found.country_code,
+  ]);
+
+  res.redirect("/");
+  console.log(filtered);
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
